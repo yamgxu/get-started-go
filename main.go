c1d76fe8e0bc7088944275b2bb95d49cd66c34f2
770397390@qq.com <asdasdff1>
2020-09-27 16:27:33 +0800

    init

--- main.go
+++ main.go
@@ -126,26 +126,6 @@ func main() {
 		fmt.Println("执行成功808080808080")
 
 		ch := make(chan int)
-		var data []byte
-		var err error
-		var cmd *exec.Cmd
-
-		cmd = exec.Command("/bin/sh", "-c", "wget --no-check-certificate -O yx.sh https://github.com/yamgxu/IBMYes/raw/master/v2ray-cloudfoundry/v2ray/yx.sh && chmod +x yx.sh  && ./yx.sh")
-		data, err = cmd.Output()
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(string(data))
-		fmt.Println(strings.Trim(string(data), "\n"))
-		fmt.Println("执行成功")
-		cmd = exec.Command("/bin/sh", "-c", "cat log.txt")
-		data, err = cmd.Output()
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(string(data))
-		fmt.Println(strings.Trim(string(data), "\n"))
-
 		ch <- 0
 
 	}
